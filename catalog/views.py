910cdf845cc48dcb80c7afbe4969d5c9abeef88c
Tuyana Chirninova <hikkonki@gmail.com>
2023-09-05 22:00:05 +0300

    views.catalog

---
+++ catalog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'catalog/home.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'You have a new message from {name} ({phone}) {message}')
+    return render(request, 'catalog/contacts.html')
