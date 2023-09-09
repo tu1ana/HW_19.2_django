afbf3ffa914a19c9cef7fc6d0d8e6d9b2a3c3501
Tuyana Chirninova <hikkonki@gmail.com>
2023-09-09 16:39:22 +0300

    product, category models added

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=250, verbose_name='Наименование')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
+    photo = models.ImageField(upload_to='products/', **NULLABLE, verbose_name='Изображение(превью)')
+    category = models.CharField(max_length=100, verbose_name='Категория')
+    price = models.FloatField(verbose_name='Цена')
+    date_created = models.DateField(verbose_name='Дата создания')
+    date_last_modified = models.DateField(verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
