bc52723e421366d6d67db65ccc1315ae25fdefc6
Tuyana Chirninova <hikkonki@gmail.com>
2023-09-18 11:03:22 +0300

    namespace added

--- config/urls.py
+++ config/urls.py
@@ -19,11 +19,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from catalog.views import home, contacts
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('catalog.urls')),
-    path('', home),
-    path('contacts/', contacts)
+    path('', include('catalog.urls', namespace='catalog')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
