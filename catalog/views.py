6ee70e261ad0eb1655126ad49f7e5cdc26cd7018
Tuyana Chirninova <hikkonki@gmail.com>
2023-09-18 11:01:19 +0300

    product added

--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
+
+from catalog.models import Product
 
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    context = {
+        'object_list': Product.objects.all(),
+        'title': 'Главная'
+    }
+    return render(request, 'catalog/home.html', context)
 
 
 def contacts(request):
+    context = {
+        'title': 'Контакты'
+    }
     if request.method == 'POST':
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
         print(f'You have a new message from {name} ({phone}) {message}')
-    return render(request, 'catalog/contacts.html')
+    return render(request, 'catalog/contacts.html', context)
+
+
+def product(request, pk):
+    context = {
+        'object_list': Product.objects.filter(pk=pk),
+        }
+    return render(request, 'catalog/product.html', context)
